d4f6e8fffeb4ccc156f08a22b2ed234c83d481f7
Richard Mitchell <richard.j.mitchell@gmail.com>
2015-08-04 16:26:05 +0100

    Add app config.

---
+++ wagtailtinymce/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailTinyMCEAppConfig(AppConfig):
+    name = 'wagtailtinymce'
+    label = 'wagtailtinymce'
+    verbose_name = "Wagtail TinyMCE editor"

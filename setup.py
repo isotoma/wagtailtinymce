eeba5e30c4a2a18c7ee51e60b2a0b507a0597823
Ben Miller <ben@hyl.co.uk>
2016-03-24 15:35:08 +0000

    version bump

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = [
 
 setup(
     name='wagtailtinymce',
-    version='4.2.1.2',
+    version='4.2.1.3',
     description='A TinyMCE editor integration for Wagtail',
     author='Richard Mitchell',
     author_email='richard.mitchell@isotoma.com',

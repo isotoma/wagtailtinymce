a02a0d4eb14c1a86d69fb4ec524495f573a2429f
riceyrice <stephen.rice@isotoma.com>
2016-05-26 10:54:49 +0100

    Switch to Wagtail 1.5 RichTextArea extension method

--- wagtailtinymce/editor.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from wagtail.wagtailcore.fields import BaseRichTextEditor
-
-
-class TinyMCEEditor(BaseRichTextEditor):
-
-    def render_js_init(self, id_, name, value):
-        return "makeTinyMCEEditable({0});".format(json.dumps(id_))

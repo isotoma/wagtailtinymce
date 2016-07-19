3f2495719c2bc1c969a64c176099170b7ea8b6e4
Jasonmate <jason@join-clima.com>
2016-07-19 17:45:51 +0800

    Fix lists span bug

--- wagtailtinymce/static/wagtailtinymce/js/tinymce-editor.js
+++ wagtailtinymce/static/wagtailtinymce/js/tinymce-editor.js
@@ -27,7 +27,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 'use strict';
 
-var mcePlugins = ['hr', 'code', 'fullscreen', 'noneditable', 'paste', 'table'],
+var mcePlugins = ['hr', 'code', 'fullscreen', 'noneditable', 'paste', 'table', 'lists'],
     mceTools = ['inserttable'],
     mceExternalPlugins = {};
 

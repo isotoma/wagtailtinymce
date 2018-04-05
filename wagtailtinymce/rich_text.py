c3fa1097178f854249a11e9b5e14b820c1f6488c
Matt Collins <mailmattcollins@gmail.com>
2018-04-05 12:54:34 +0100

    Update TinyMCERichTextArea to use EditorHTMLConverter for wagtail 2

--- wagtailtinymce/rich_text.py
+++ wagtailtinymce/rich_text.py
@@ -35,8 +35,8 @@ from wagtail import __version__
 
 if __version__ >= '2.0':
     from wagtail.admin.edit_handlers import RichTextFieldPanel
-    from wagtail.admin.rich_text.converters.editor_html import DbWhitelister
-    from wagtail.core.rich_text import expand_db_html
+    from wagtail.admin.rich_text.converters.editor_html import EditorHTMLConverter
+    from wagtail.core.rich_text import features
 else:
     from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
     from wagtail.wagtailcore.rich_text import DbWhitelister
@@ -72,8 +72,16 @@ class TinyMCERichTextArea(WidgetWithScript, widgets.Textarea):
     def __init__(self, attrs=None, **kwargs):
         super(TinyMCERichTextArea, self).__init__(attrs)
         self.kwargs = self.getDefaultArgs()
+        self.features = kwargs.pop('features', None)
         if kwargs is not None:
             self.kwargs.update(kwargs)
+
+        if __version__ >= '2.0':
+            if self.features is None:
+                self.features = features.get_default_features()
+                self.converter = EditorHTMLConverter()
+            else:
+                self.converter = EditorHTMLConverter(self.features)
 
     def get_panel(self):
         return RichTextFieldPanel
@@ -82,7 +90,10 @@ class TinyMCERichTextArea(WidgetWithScript, widgets.Textarea):
         if value is None:
             translated_value = None
         else:
-            translated_value = expand_db_html(value, for_editor=True)
+            if __version__ >= '2.0':
+                translated_value = self.converter.from_database_format(value)
+            else:
+                translated_value = expand_db_html(value, for_editor=True)
         return super(TinyMCERichTextArea, self).render(name, translated_value, attrs)
 
     def render_js_init(self, id_, name, value):
@@ -111,4 +122,8 @@ class TinyMCERichTextArea(WidgetWithScript, widgets.Textarea):
         original_value = super(TinyMCERichTextArea, self).value_from_datadict(data, files, name)
         if original_value is None:
             return None
-        return DbWhitelister.clean(original_value)
+
+        if __version__ >= '2.0':
+            return self.converter.to_database_format(original_value)
+        else:
+            return DbWhitelister.clean(original_value)

44e12c90831c50b4b1640f82915d8819a5ade69d
Samuel Mendes <samuel.mendes@gmail.com>
2019-03-05 11:55:52 +0100

    Bump package version to 4.7.13.4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = [
 
 setup(
     name='wagtailtinymce',
-    version='4.7.13.3',
+    version='4.7.13.4',
     description='A TinyMCE editor integration for Wagtail',
     author='Richard Mitchell',
     author_email='richard.mitchell@isotoma.com',
